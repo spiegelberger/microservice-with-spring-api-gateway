35bd6c780457939b7ee6d3c2d4c01ca283d8ff78
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-05-16 07:50:03 +0200

    Microservice projects are created.

---
+++ PhotoAppApiUsers/src/main/java/photoapp/api/users/ui/controllers/UsersController.java
@@ -0,0 +1,20 @@
+package photoapp.api.users.ui.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	@Autowired
+	private Environment environment;
+
+	@GetMapping("/status/check")
+	public String status() {
+		return "UsersController is working on port " + environment.getProperty("local.server.port");
+	}
+}
