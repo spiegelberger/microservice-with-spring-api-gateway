736e1fd0f1677301321fdac39f7713fde8f50aba
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-05-20 07:16:34 +0200

    Config Server created.

---
+++ PhotoAppApiConfigServer/src/test/java/photoapp/api/config/server/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package photoapp.api.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
